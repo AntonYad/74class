c5199c3521d6f25257ed8fd26eabadfcf0994fae
AntonYad <651299847@qq.com>
2020-09-04 21:01:10 +0800

    作业

---
+++ Project23/Project23/源.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+int fib(int n)
+{
+	int result;
+	int pre_result;
+	int next_older_result;
+	result = pre_result = 1;
+	while (n > 2)
+	{
+		n -= 1;
+		next_older_result = pre_result;
+		pre_result = result;
+		result = pre_result + next_older_result;
+	}
+	return result;
+}
