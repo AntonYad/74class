77df4c1558f22732398a136ef0a30be43e8617fd
AntonYad <651299847@qq.com>
2020-09-17 14:57:10 +0800

    作业

---
+++ test8.20/test8.20/源.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	printf("请输入两个整数:\n");
+	scanf("%d %d", &a &b);
+	c = (a > b )? b: a;
+	if (a%c1！ = 0 || b%c != 0)
+	{
+		c--；
+	}
+	return 0；
+
+}
