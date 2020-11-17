2bae6d53b2754a9bb9b7316d560a1a90711908a2
Anton <651299847@qq.com>
2020-11-17 17:15:55 +0800

    数字三角形

---
+++ 数字三角形/数字三角形/数字三角形.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	int i, j;
+	while (scanf("%d", &n) != EOF)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				printf("%d ", j);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
